f4ea48ed51b63c986699b5b5fe4e8d78729cbaac
Nash Kaminski <nashkaminski@comcast.net>
2016-07-04 01:10:46 -0500

    Use char-write-req as opposed to char-write for writing the attribute, fixes hang and communication failure on devices other than the Raspberry Pi 3

--- csrmesh/gatt.py
+++ csrmesh/gatt.py
@@ -5,6 +5,6 @@ from binascii import hexlify
 def gatt_write(dest, handle, data):
 	strdata = hexlify(data).decode('ascii')
 	p_handle = "0x" + hexlify(handle).decode('ascii')
-	cmd = "gatttool -b %s --char-write -a %s -n %s" % (dest, p_handle, strdata)
+	cmd = "gatttool -b %s --char-write-req -a %s -n %s" % (dest, p_handle, strdata)
 	print("Running " + cmd)
 	os.system(cmd)
