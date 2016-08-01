8e3326f152ac6ad0e36adfc9125988fb549ccda5
Nash Kaminski <nashkaminski@gmail.com>
2016-08-01 03:01:03 -0500

    Add packet decrypt functions, network key bruteforce, and variable length payload handling

--- csrmesh/__init__.py
+++ csrmesh/__init__.py
@@ -2,13 +2,13 @@ import hmac
 import hashlib
 import struct
 import random
+from binascii import hexlify
 from . import gatt
 from Crypto.Cipher import AES
 
 def network_key(pin):
     #Derives the long term network key(128 bits) from the 4 digit setup PIN
     h = hashlib.sha256()
-    pin = str(pin)
     pin2 = pin.encode('ascii') + b'\x00MCP'
     h.update(pin2)
     dig = bytearray(h.digest())
@@ -18,20 +18,66 @@ def network_key(pin):
 def make_packet(key,seq,data):
     magic = b'\x80'
     eof = b'\xff'
+    dlen = len(data)
     enc = AES.new(key, AES.MODE_ECB)
     #Compute "base" based on the sequence number and encrypt with net key
     base = struct.pack("<Ixc10x",seq,magic)
-    ebase = enc.encrypt(base)[:10]
+    ebase = enc.encrypt(base)[:dlen]
     #XOR the encrypted base with the data
     payload = bytes([ a ^ b for (a,b) in zip(data, ebase) ])
     #Now pad, combine with header and compute HMAC
-    prehmac = struct.pack("<8xIc10s",seq,magic,payload)
+    prehmac = struct.pack("<8xIc"+str(dlen)+"s",seq,magic,payload)
     hm = bytearray(hmac.new(key, msg=prehmac, digestmod=hashlib.sha256).digest())
     #Reverse the order of the bytes and truncate
     hm.reverse()
     hm = bytes(hm)[:8]
-    final = struct.pack("<Ic10s8sc",seq,magic,payload,hm,eof)
+    final = struct.pack("<Ic"+str(dlen)+"s8sc",seq,magic,payload,hm,eof)
     return final
+
+def decrypt_packet(key, data):
+    if(len(data)<14):
+        return None
+    od = {}
+    dlen = len(data)-14
+    (seq, magic, epayload, hmac_packet, eof) = struct.unpack("<Ic"+str(dlen)+"s8sc", data)
+    #Pad, combine with header and compute HMAC
+    prehmac = struct.pack("<8xIc10s",seq,magic,epayload)
+    hm = bytearray(hmac.new(key, msg=prehmac, digestmod=hashlib.sha256).digest())
+    #Reverse the order of the bytes and truncate
+    hm.reverse()
+    hmac_computed = bytes(hm)[:8]
+    #Begin decryption
+    enc = AES.new(key, AES.MODE_ECB)
+    #Compute "base" based on the sequence number and encrypt with net key
+    base = struct.pack("<Ixc10x",seq,magic)
+    ebase = enc.encrypt(base)[:dlen]
+    #XOR the encrypted base with the data
+    decpayload = bytes([ a ^ b for (a,b) in zip(epayload, ebase) ])
+    od['seq']=seq
+    od['magic']=magic
+    od['encpayload']=epayload
+    od['decpayload']=decpayload
+    od['hmac_computed']=hmac_computed
+    od['hmac_packet']=hmac_packet
+    od['eof']=eof
+    return od
+
+def network_key_bruteforce(data):
+    #Cracks the 4 digit pin given the contents of a packet in a few seconds.
+    for x in range(0,10000):
+        pinstr="{:04X}".format(x)
+        ki = network_key(pinstr)
+        trial = decrypt_packet(ki, data)
+        if(trial['hmac_computed'] == trial['hmac_packet']):
+            return (pinstr, trial)
+    return None
+
+def print_decrypted_packet(decpkt):
+    for k in decpkt.keys():
+        if(type(decpkt[k]) is bytes):
+            print(k+": "+hexlify(decpkt[k]).decode('ascii'))
+        else:
+            print(k+": "+str(decpkt[k]))
 
 def light_set_cmd(level, red, green, blue):
     cmd = b'\x73\x11'
