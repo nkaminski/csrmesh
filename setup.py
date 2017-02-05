545c9ecc762101e5bcf1f38f7041045220c07201
Tomas Salfischberger <t.salfischberger@relay42.com>
2017-02-05 15:25:32 +0100

    Updated CLI tool, README and package info for MOVE addition

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='csrmesh',
-        version='0.7.0',
+        version='0.8.0',
         description='Reverse engineered implementation of the CSRMesh bridge protocol',
-        long_description='Reverse engineered implementation of the CSRMesh bridge protocol. Currently only capable of interfacing with Feit HomeBrite smart bulbs but support for additional devices can easily be added. Requires bluez gatttool to transmit packets.',
+        long_description='Reverse engineered implementation of the CSRMesh bridge protocol. Currently only capable of interfacing with Feit HomeBrite smart bulbs and MOVE controller. Support for additional devices can easily be added. Requires bluez gatttool to transmit packets.',
         url='https://github.com/nkaminski/csrmesh',
         classifiers=[
         'Development Status :: 3 - Alpha',
@@ -14,8 +14,8 @@ setup(name='csrmesh',
         'Topic :: Home Automation',
         ],
         keywords='bluetooth csrmesh qualcomm csr BTLE',
-        author='Nash Kaminski',
-        author_email='nashkaminski@kaminski.io',
+        author='Nash Kaminski, Tomas Salfischberger',
+        author_email='nashkaminski@kaminski.io, tomas@salfischberger.nl',
         license='GPLv3',
         packages=['csrmesh'],
         install_requires=[
